175cc4ea533142cb12a59e855de2f1edf3ae558c
Shadelle Brown <brownshadelle77@gmail.com>
2025-10-17 22:30:35 -0500

    Exercise 2 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // all div elements inside the board
     const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';
+    const gameState = ['', '', '', '', '', '', '', '', ''];
     
-    // square class added to each square
-    squares.forEach(square => {
+    squares.forEach((square, index) => {
         square.classList.add('square');
+        
+        // click event
+        square.addEventListener('click', function() {
+            if (square.textContent === '') {
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                gameState[index] = currentPlayer;
+                
+                // player switched
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     });
 });
